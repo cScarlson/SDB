9b5f1c054217fc61c9ec264d7f7d670379e0b173
cScarlson <otocarlson@gmail.com>
2013-03-17 12:14:08 -0600

    modded somethings and added implementation step to create subObjects inside of indices

--- SDB.js
+++ SDB.js
@@ -27,13 +27,21 @@ window.sdb = (window.sdb || (function(){
 				);
 				createIndices(stores[store].indices);
 				
-				/* TEST objectStore given that name
-				 * ...and email are part of the schema. */
+				// TEST objectStore given that name and email are part of the schema.
+				/*
 				var peopleData = [
-					{ name: "John Dow", email: "john@company.com" },
-					{ name: "Don Dow", email: "don@company.com" }
+					{name: 'John Dow', email: 'john@company.com'},
+					{name: 'Don Dow', email: 'don@company.com'},
+					{name: 'versions', email: 'unique@email.com', versions: [
+							{versionName: 'myVersionName1', pubKey: 'myPubKey1'},
+							{versionName: 'myVersionName2', pubKey: 'myPubKey2'},
+							{versionName: 'myVersionName3', pubKey: 'myPubKey3'}
+						]
+					}
 				];
-				for(i in peopleData){objectStore.add(peopleData[i]);}/* END TEST */
+				for(i in peopleData){objectStore.add(peopleData[i]);} //END TEST
+				*/
+				
 			}
 			
 			function createIndices(inds){
@@ -66,7 +74,7 @@ window.sdb = (window.sdb || (function(){
 	
 	SDB.prototype = (function(){
 		var transaction, objectStore;
-		function transaction(db, store, transactionType){
+		function createTransaction(db, store, transactionType){
 			console.log('hit trans', db, store, IDBTransaction[transactionType]);
 			transaction = db.transaction(store, IDBTransaction[transactionType]);
 			console.log('transaction', transaction);
@@ -74,9 +82,7 @@ window.sdb = (window.sdb || (function(){
 		};
 		
 		function objectStore(store){
-			console.log('#transaction', transaction);
-			objectStore = transaction.objectStore(store);
-			console.log('#objectStore', objectStore);
+			(transaction) && (objectStore = transaction.objectStore(store));
 			return this;
 		};
 		
@@ -156,14 +162,6 @@ window.sdb = (window.sdb || (function(){
 				return this;
 			};
 			
-			function indexPUT(obj, callback){
-				storeIndex.put(obj).onsuccess = function(e){
-					var result = e.target.result;
-					callback(result);
-				};
-				return this;
-			};
-			
 			function openCursor(callback){
 				var cursor, items = [], req = storeIndex.openCursor();
 				req.onsuccess = function(e){
@@ -192,7 +190,6 @@ window.sdb = (window.sdb || (function(){
 			
 			return {
 				get: indexGET,
-				put: indexPUT,
 				openCursor: openCursor,
 				openKeyCursor: openKeyCursor
 			};
@@ -207,7 +204,7 @@ window.sdb = (window.sdb || (function(){
 		 * @return methods to 'this'
 		 */
 		return {
-			trans: transaction,
+			trans: createTransaction,
 			store: objectStore,
 			add: ADD,
 			put: PUT,
@@ -229,7 +226,7 @@ window.sdb = (window.sdb || (function(){
 	
 }()));
 
-var schema = {
+var PeopleDBschema = {
 	db:'PeopleDB',
 	v: 1,
 	upgrade: {
@@ -262,11 +259,11 @@ var schema = {
 	} 
 };
 
-var idb = sdb.req(schema, function(db){
+var idb = sdb, PeopleDB = idb.req(PeopleDBschema, function(db){
 
 	/**/
 	console.log('success!', db, '\n\n');
-	idb
+	PeopleDB
 		.tr(db, ['people'], 'READ_WRITE')
 		.store('people')
 		.add()
@@ -277,8 +274,8 @@ var idb = sdb.req(schema, function(db){
 		.get('1', function(item){
 			console.log('GOT ITEM', item);	
 			});
-		
-	idb
+			
+	PeopleDB
 		.tr(db, ['aliens'], 'READ_WRITE')
 		.store('aliens')
 		.add()
@@ -291,21 +288,21 @@ var idb = sdb.req(schema, function(db){
 			});
 			
 	// store.openCursor()
-	idb.tr(db, ['people'], 'READ_WRITE')
+	PeopleDB.tr(db, ['people'], 'READ_WRITE')
 		.store('people')
 		.cursor(function(value){
 			console.log('openCursor callback:: cursor value:', value);
 			});
 			
 	// store.openCursor()
-	idb.tr(db, ['aliens'], 'READ_WRITE')
+	PeopleDB.tr(db, ['aliens'], 'READ_WRITE')
 		.store('aliens')
 		.cursor(function(value){
 			console.log('openCursor callback:: cursor value:', value);
 			});
 			
 	// index.get()
-	idb.tr(db, ['people'], 'READ_WRITE')
+	PeopleDB.tr(db, ['people'], 'READ_WRITE')
 		.store('people')
 		.index('name')
 			.get('cody', function(result){
@@ -316,13 +313,10 @@ var idb = sdb.req(schema, function(db){
 			})
 			.openKeyCursor(function(result){
 			console.log('index.openKeyCursor(), people: result:', result);
-			})
-			.put({name: 'cody2', email: 'oto2@gmail.com'}, function(item){
-			console.log('index.PUT ITEM', item);
 			});
 			
 	// index.get()
-	idb.tr(db, ['aliens'], 'READ_WRITE')
+	PeopleDB.tr(db, ['aliens'], 'READ_WRITE')
 		.store('aliens')
 		.index('name')
 			.get('codius', function(result){
@@ -333,16 +327,31 @@ var idb = sdb.req(schema, function(db){
 			})
 			.openKeyCursor(function(result){
 			console.log('index.openKeyCursor(), aliens: result:', result);
-			})
-			.put({name: 'coduis2', email: 'codius2@gmail.com'}, function(item){
-			console.log('index.PUT ITEM', item);
 			});
+			
+	PeopleDB
+		.tr(db, ['people', 'aliens'], 'READ_WRITE')
+		.store('people')
+		.put({name: 'versions', email: 'unique2@email.com', versions: [
+				{versionName: 'myOtherVersionName1', pubKey: 'myOtherPubKey1'},
+				{versionName: 'myOtherVersionName2', pubKey: 'myOtherPubKey2'},
+				{versionName: 'myOtherVersionName3', pubKey: 'myOtherPubKey3'}
+			]
+		})
+		.tr(db, ['people', 'aliens'], 'READ_WRITE')
+		.store('aliens')
+		.put({name: 'versions', email: 'unique2@email.com', versions: [
+				{versionName: 'myOtherVersionName1', pubKey: 'myOtherPubKey1'},
+				{versionName: 'myOtherVersionName2', pubKey: 'myOtherPubKey2'},
+				{versionName: 'myOtherVersionName3', pubKey: 'myOtherPubKey3'}
+			]
+		});
 	
 	/**/
 	
 });
 
-console.log('idb', idb, '\n\n');
+console.log('idb', PeopleDB, '\n\n');
 
 
 
